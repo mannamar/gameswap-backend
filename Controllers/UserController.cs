6e0af5f2e5ab8442ec1f91cacaccfdd0a05eba85
Amardeep Mann <amar2slick@gmail.com>
2023-04-14 14:48:23 -0700

    Add User Endpoint Created (Mid Lec 6)

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using gameswap_backend.Models.DTO;
+using gameswap_backend.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gameswap_backend.Controllers
@@ -10,6 +12,15 @@ namespace gameswap_backend.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        
+        private readonly UserService _data;
+        public UserController(UserService data)
+        {
+            _data = data;
+        }
+        // Add a user
+        public bool AddUser(CreateAccountDTO UserToAdd)
+        {
+            return _data.AddUser(UserToAdd);
+        }
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -2,11 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using gameswap_backend.Models.DTO;
+using gameswap_backend.Services.Context;
 
 namespace gameswap_backend.Services
 {
     public class UserService
-    {
-        
+    {   
+        private readonly DataContext _context;
+        public UserService(DataContext context)
+        {
+            _context = context;
+        }
+        public bool DoesUserExist(string Username)
+        {
+            // Check the table to see if the username exists
+                // If one item matches the condition return the item
+                // If no item matches the condition return null
+                // If multipel items match an error will occur
+            return _context.UserInfo.SingleOrDefault(user => user.Username == Username) != null;
+        }
+
+        public bool DoesEmailExist(string Email)
+        {
+            return _context.UserInfo.SingleOrDefault(user => user.Email == Email) != null;
+        }
+        public bool AddUser(CreateAccountDTO UserToAdd)
+        {
+            // Check if the user exists
+            // If the user does not exist
+                // Create the account
+            if (!DoesUserExist(UserToAdd.Username))
+        }
     }
 }
