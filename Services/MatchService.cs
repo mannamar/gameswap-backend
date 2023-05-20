7d8e4355354cc667ecea8b59fa521d2939209514
Amardeep Mann <amar2slick@gmail.com>
2023-05-20 16:59:11 -0700

    One for loop left

--- Services/MatchService.cs
+++ Services/MatchService.cs
@@ -16,7 +16,28 @@ namespace gameswap_backend.Services
         }
 
         public dynamic GetMatches(int userId){
-            return _context.WishListItemInfo.Where(item => (item.UserId == userId && item.isDeleted == false));
+            // Get user's active WishList
+            var userWishItems = _context.WishListItemInfo.Where(item => (item.UserId == userId && item.isDeleted == false)).ToList();
+            
+            dynamic result = "";
+            List<string> myList = new List<string>();
+
+            // For each item in the user's wishlist
+            foreach (var wishItem in userWishItems)
+            {
+                // Pull out a list of what that user would trade
+                var trades = _context.TradeItemInfo.Where(item => (item.WishListItemId == wishItem.Id && item.isDeleted == false)).ToList();
+                // For each item they would trade
+                foreach (var tradeItem in trades)
+                {
+                    // Find who else wants those item
+                    var matches = _context.WishListItemInfo.Where(item => (item.IgdbId == tradeItem.IgdbId)).ToList();
+                    string res = "Trade Id " + tradeItem.Id + ": " + wishItem.GameName + " - " + tradeItem.GameName;
+                    myList.Add(res);
+                    result = matches;
+                }
+            }
+            return result;
         }
     }
 }
